3882f389bda8378582ecdab63d384e2d2e08c24b
Barcot <barcotg@gmail.com>
2022-10-27 23:58:51 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,14 @@
-#include <studio.h>
+#include <stdio.h>
 
- /*
+/**
+ * main - Entry Point
  *
- * main - Entry point
- *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
-
-	    puts ("\"Programming is like building a multilingual puzzle");
-	    return (0);
-	  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
