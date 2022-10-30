7b3acfd2d21903820c7c756a00fc7c70029afaa5
Barcot <barcotg@gmail.com>
2022-10-30 11:33:26 -0700

    task 13

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print sum of Fibanocci numbers
+ * less than 6000000.
+ *
+ * Return: Nothing!
+ */
+
+int main(void)
+{
+	int i = 0;
+	long e = i, g = 2, sum = g;
+	
+	while ( g + e < 6000000)
+	{
+		g += e;
+
+		if (g % 2 == 0)
+			sum += g;
+
+		e = g - e;
+
+		++i;
+	}
+
+	printf("%ld\n", sum);
+	return (0);
+}
