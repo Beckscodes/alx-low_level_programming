68299f993da2495ab1d041ff98a580107899663d
Barcot <barcotg@gmail.com>
2022-10-30 13:16:39 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 0 to 9
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
