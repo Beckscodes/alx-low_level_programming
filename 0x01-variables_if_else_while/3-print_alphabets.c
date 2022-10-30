9efcf71bb85e9ec85b0e603f296928c8cabe8459
Barcot <barcotg@gmail.com>
2022-10-30 12:56:23 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		putchar(d);
+	}
+
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+		putchar(d);
+	}
+
+	utchar('\n');
+
+	return(0);
+}
+
