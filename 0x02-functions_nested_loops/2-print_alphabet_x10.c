bbc6c4e016770e99fed0bc3ac423e62a85b1440b
Barcot <barcotg@gmail.com>
2022-10-30 06:46:59 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet_x10
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+
+}
