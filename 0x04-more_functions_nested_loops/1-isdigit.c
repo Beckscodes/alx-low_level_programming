ef28322d8d70b7020820f3d358339a628775fc90
Barcot <barcotg@gmail.com>
2022-10-30 16:32:56 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if cha is a digit
+ * @c: int to be checked
+ * Return: 1 if c is a digit, else 0
+ */
+int _isdigit(int c)
+{
+	int x = 0;
+
+	if (c >= '0' && c <= '9')
+		x = 1;
+
+	return (x);
+}
